d9038fe1b547f07b7d056aacd6cfcf45db176bbe
anikanagle <nikunagle@gmail.com>
2025-09-29 15:23:31 -0400

    Breakbeam stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/Intake.java
@@ -9,19 +9,27 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+
 @Config
 public class Intake {
     public DcMotorEx intakeMotor;
     public CRServo toilet3;
     public static double power = 0.0;
     public static double betterPower = 0.0;
+
+    DigitalChannel breakBeam;
+
+
     public Intake (OpMode opMode){
         intakeMotor.setDirection(DcMotorEx.Direction.FORWARD);
         intakeMotor = opMode.hardwareMap.get(DcMotorEx.class, "intake motor");
         toilet3 = opMode.hardwareMap.get(CRServo.class,"toilet roll 3");
+        breakBeam = opMode.hardwareMap.get(DigitalChannel.class, "BreakBeam");
     }
 
-    public void intake(){
+    public void intake_without_sense(){
         intakeMotor.setDirection(DcMotorEx.Direction.FORWARD);
         intakeMotor.setPower(power);
         toilet3.set(betterPower);
@@ -36,7 +44,13 @@ public class Intake {
     public void stopIntake(){
         intakeMotor.setPower(0);
         toilet3.set(0);
+    }
 
+
+    public boolean getSensorState(){
+        return breakBeam.getState();
+        // true= unbroken
+        // false = broken
     }
 
 }
