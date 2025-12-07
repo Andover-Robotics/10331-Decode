f712c613531424a77ab33fc14f0817460bdd45e8
purpleflower230 <sahithyasrinath12@gmail.com>
2025-12-07 17:02:35 -0500

    auto lighninh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LightningAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LightningAutoBlue.java
@@ -34,13 +34,13 @@ public class LightningAutoBlue extends LinearOpMode {
     //decrease x decrease y
 
     //intake
-    public static Pose2d firstIntake1 = new Pose2d(16,-46,Math.toRadians(-85));//,Math.toRadians(-180)
+    public static Pose2d firstIntake1 = new Pose2d(16,-57,Math.toRadians(-85));//,Math.toRadians(-180)
     public static Vector2d firstIntake2 = new Vector2d(16,-65);//,Math.toRadians(-180)
 
-    public static Pose2d secondIntake1 = new Pose2d(-8,-46,Math.toRadians(-85));
+    public static Pose2d secondIntake1 = new Pose2d(-8,-57,Math.toRadians(-85));
     public static Vector2d secondIntake2 = new Vector2d(-8,-65);
 
-    public static Vector2d gatePos = new Vector2d(3,-75);
+    public static Vector2d gatePos = new Vector2d(5,-83);
     public static Pose2d thirdIntake = new Pose2d(-40,-60,Math.toRadians(-90));
     public ExposureControl exposureControl;
     public GainControl gainControl;
@@ -75,14 +75,15 @@ public class LightningAutoBlue extends LinearOpMode {
                 .afterTime(0.01,bot.actionStopShoot())
                 .stopAndAdd(new InstantAction(()->bot.intake.stopIntake()))
 
-                .setTangent(Math.toRadians(90))
+                .setTangent(Math.toRadians(-50))
                 .splineToLinearHeading(firstIntake1, Math.toRadians(-60))//intake1
                 .afterTime(0.01,bot.intake.actionIntake())
                 .strafeToLinearHeading(firstIntake2,Math.toRadians(-85))
                 .afterTime(0.01,new InstantAction(()->bot.intake.stopIntake()))
 
-                .setReversed(true)
-                .splineToLinearHeading(new Pose2d(gatePos, Math.toRadians(0)), Math.toRadians(-120))
+                .setTangent(Math.toRadians(90))
+//                .splineTo(new Vector2d(16,-50),Math.toRadians(-90)) - added just now
+                .splineToLinearHeading(new Pose2d(gatePos, Math.toRadians(15)), Math.toRadians(90))
 
 //                .strafeToLinearHeading(new Vector2d(16,-50),Math.toRadians(0))
 //                .strafeToLinearHeading(gatePos,Math.toRadians(15))//gate
@@ -95,20 +96,20 @@ public class LightningAutoBlue extends LinearOpMode {
                 .waitSeconds(2)
                 .afterTime(0.01,bot.actionStopShoot())
 
-//                .setTangent(Math.toRadians(90))
-//                .splineToLinearHeading(secondIntake1, Math.toRadians(-10))//intake2
-//                .afterTime(0.01,bot.intake.actionIntake())
-//                .strafeToLinearHeading(secondIntake2,Math.toRadians(-85))
-//                .afterTime(0.01,new InstantAction(()->bot.intake.stopIntake()))
-//
-//
-//                .setTangent(Math.toRadians(90)) //shoot 3
-//                .afterTime(0.01,bot.intake.actionIntake())
-//                .splineToLinearHeading(shoot,Math.toRadians(60))
-//                .stopAndAdd(bot.actionShoot())
-//                .waitSeconds(3)
-//                .stopAndAdd(bot.actionStopShoot())
-//                .waitSeconds(1)
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(secondIntake1, Math.toRadians(-60))//intake2
+                .afterTime(0.01,bot.intake.actionIntake())
+                .strafeToLinearHeading(secondIntake2,Math.toRadians(-85))
+                .afterTime(0.01,new InstantAction(()->bot.intake.stopIntake()))
+
+
+                .setTangent(Math.toRadians(90)) //shoot 3
+                .afterTime(0.01,bot.intake.actionIntake())
+                .splineToLinearHeading(shoot,Math.toRadians(60))
+                .stopAndAdd(bot.actionShoot())
+                .waitSeconds(3)
+                .stopAndAdd(bot.actionStopShoot())
+                .waitSeconds(1)
 
 
 
@@ -121,24 +122,11 @@ public class LightningAutoBlue extends LinearOpMode {
 
 
         waitForStart();
-
-        while(opModeIsActive()) {
-            telemetry.addData("id:", bot.aprilTag.getId());
-            telemetry.update();
-
-            int i =0;
-            while(i<1) {
-                Actions.runBlocking(
-                        new ActionHelper.RaceParallelCommand(
-                                bot.actionPeriodic(),
-                                runAuto
-                        ));
-                i++;
-            }
-        }
-
-
-
+        Actions.runBlocking(
+                new ActionHelper.RaceParallelCommand(
+                        bot.actionPeriodic(),
+                        runAuto
+                ));
 
     }
 }
