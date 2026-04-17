e17e3da7dfe5f849acb10d07636ab355f55ce846
purpleflower230 <sahithyasrinath12@gmail.com>
2026-04-17 08:44:14 -0400

    wraparound

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Subsystems/Turret.java
@@ -88,6 +88,7 @@ public class Turret {
         if (angle < -210) angle += 330; //low limit
         if (angle >= 120) angle -= 330; // high limit
         //djfoeijfoewijfaoie
+        //fiuahiauefia
         return angle;
     }
     /*public double wrapAround(double angle) {
